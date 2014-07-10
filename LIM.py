43526e8ed8d2c2a05b055de200648aab6400fb98
Andre Perkins <frodre@gmail.com>
2014-07-10 16:34:02 -0700

    [Stats.py] In the process of upgrading runMean to use netcdf operators.  Just finished the calculation of running average.

--- LIM.py
+++ LIM.py
@@ -12,23 +12,26 @@ monthly mean format.
 import numpy as np
 from scipy.io import netcdf as ncf
 from scipy.signal import convolve
+import Stats as st
 
-data_file = '/home/melt/wperkins/data/20CR/air.2m.mon.mean.nc'
-window_size = 12
+data_file = '/home/melt/wperkins/data/ccsm4_last_mil/tas_Amon_CCSM4_past1000_r1i1p1_085001-185012.nc'
+wsize = 12
 
 #Load netcdf file
 f = ncf.netcdf_file(data_file, 'r')
-tdata = f.variables['air'].data
+#tdata = f.variables['air'].data
+tdata = f.variables['tas'].data#[0:1692, :, :]
 dshape = tdata.shape
 num_years = dshape[0]/12
 shp_tdata = tdata.reshape(num_years, 12, dshape[1], dshape[2]) #reshape data
 
 #Calc monthly climatological mean from data
-mon_climo = np.sum(shp_tdata, axis=0) / float(num_years)
+print "Calculating monthly climatology..."
+mon_climo = ncf.np.sum(shp_tdata, axis=0) / float(num_years)
+print "Done!"
 
 #Calc running mean using window size over the data
-#avg_arr = np.ones((window_size, dshape[1], dshape[2]))/float(window_size)
-#run_mean = convolve(tdata, avg_arr, 'same')
+run_mean, bedge, tedge = st.runMean(tdata, wsize)
 
 neigs = 20
 
--- Stats.py
+++ Stats.py
@@ -4,8 +4,10 @@ Toolbox for statistical methods.
 """
 
 import numpy as np
+import threading
+import subprocess
 
-def runMean(data, window_size, threading=False, num_threads=None):
+def runMean(nc_file, nc_varname, window_size, threading=False, num_threads=None, useNCO=False):
     """
     A function for calculating the running mean on data.
 
@@ -22,6 +24,10 @@ def runMean(data, window_size, threading=False, num_threads=None):
         valid if threading is set to True.  If value is not 
         specified and threading is requested, the default value 
         is 4.
+    useNCO: bool, optional
+        If the calculation is over a large dataset where memory use
+        may be an issue, the use of netCDF operators is likely a good
+        idea.  It will speed up the process and save memory...
 
     Returns
     -------
@@ -37,16 +43,29 @@ def runMean(data, window_size, threading=False, num_threads=None):
         num_threads = 4
 
     #Calculate edges of running mean calculatable from given data
+    data = nc_file.variables[nc_varname].data
     dshape = data.shape
-    top_edge = window_size/2
+    top_edge = window_size/2 + 1
     bot_edge = (window_size/2) + (window_size%2) - 1
     tot_cut = top_edge + bot_edge
     new_shape = list(dshape)
     new_shape[0] -= tot_cut
-    result = np.zeros(new_shape)
 
-    for cntr in range(new_shape[0]):
-        result[cntr] = data[(cntr-bot_edge):(cntr+top_edge),:,:].sum(axis=0)
-    result = result/float(window_size)
+    if useNCO:
+        inFile = nc_file.filename 
+        for idx in range(new_shape[0]):
+            outFile = '.'.join([inFile, str(idx), 'tmp'])
+            execArgs = ['ncra', '-d', 'time,%i,%i'%(idx, idx+window_size), inFile, outFile]
+            p = subprocess.Popen(execArgs)
+            p.wait()
+            
+    else:
+        result = np.zeros(new_shape)
+
+        for cntr in range(new_shape[0]):
+            print 'Calc for index %i' % cntr
+            result[cntr] = data[(cntr-bot_edge):(cntr+top_edge)].sum(axis=0)
+        result = result/float(window_size)
+    
     return (result, bot_edge, top_edge)
     
