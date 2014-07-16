549be67f1c6970e5af17c68a9399f7ee1ef363b2
Andre Perkins <frodre@gmail.com>
2014-07-16 11:08:51 -0700

    [Stats.py, LIM.py] Finished the addition of option to utilize netCDF operators.  Running means appears to work, but more testing is necessary.

--- LIM.py
+++ LIM.py
@@ -13,25 +13,49 @@ import numpy as np
 from scipy.io import netcdf as ncf
 from scipy.signal import convolve
 import Stats as st
+import matplotlib.pyplot as plt
+from time import time
 
-data_file = '/home/melt/wperkins/data/ccsm4_last_mil/tas_Amon_CCSM4_past1000_r1i1p1_085001-185012.nc'
+plt.close('all')
+
+#data_file = '/home/melt/wperkins/data/ccsm4_last_mil/tas_Amon_CCSM4_past1000_r1i1p1_085001-185012.nc'
+data_file = '/home/melt/wperkins/data/20CR/air.2m.mon.mean.nc'
 wsize = 12
+var_name = 'air'
 
 #Load netcdf file
 f = ncf.netcdf_file(data_file, 'r')
-#tdata = f.variables['air'].data
-tdata = f.variables['tas'].data#[0:1692, :, :]
-dshape = tdata.shape
-num_years = dshape[0]/12
-shp_tdata = tdata.reshape(num_years, 12, dshape[1], dshape[2]) #reshape data
-
-#Calc monthly climatological mean from data
-print "Calculating monthly climatology..."
-mon_climo = ncf.np.sum(shp_tdata, axis=0) / float(num_years)
-print "Done!"
+tvar = f.variables[var_name]
+try:
+    sf = tvar.scale_factor
+    offset = tvar.add_offset
+    tdata = tvar.data*sf + offset
+except AttributeError:
+    tdata = tvar.data
 
 #Calc running mean using window size over the data
-run_mean, bedge, tedge = st.runMean(tdata, wsize)
+print "\nCalculating running mean..."
+t1 = time()
+run_mean, bedge, tedge = st.runMean(f, var_name, wsize, num_procs=2, useNCO=True)
+t2 = time()
+dur = t2 - t1
+print "Done! (Completed in %f s)" % dur
+
+new_shp = [run_mean.shape[0]/wsize, wsize] + list(run_mean.shape[1:])
+shp_run_mean = run_mean.reshape( new_shp )
+
+print "\nCalculating monthly climatology from running mean..."
+mon_climo = np.sum(shp_run_mean, axis=0)/float(new_shp[0])
+print "Done!"
+
+anomaly_srs = (shp_run_mean - mon_climo).reshape(([new_shp[0]*wsize] + new_shp[2:]))
+x = range(len(anomaly_srs))
+
+fig, ax = plt.subplots(2,1)
+ax[0].plot(x, anomaly_srs[:,30,30])
+ax[1].plot(x, run_mean[:,30,30])
+fig.show()
 
 neigs = 20
 
+f.close()
--- Stats.py
+++ Stats.py
@@ -4,30 +4,30 @@ Toolbox for statistical methods.
 """
 
 import numpy as np
-import threading
-import subprocess
+import scipy.io.netcdf as ncf
+from subprocess import call, Popen
+from multiprocessing import Pool
+from os.path import exists, join, splitext
 
-def runMean(nc_file, nc_varname, window_size, threading=False, num_threads=None, useNCO=False):
+def runMean(nc_file, nc_varname, window_size, num_procs=None, useNCO=False):
     """
     A function for calculating the running mean on data.
 
     Parameters
     ----------
-    data: ndarray
-        Data to compute the running mean over.
+    nc_file: scipy.io.netcdf.netcdf_file
+        NetCDF file pointer for desired dataset.
+    nc_varname: str
+        Dictionary key of data to be averaged
     window_size: int
         Size of the window to compute the running mean over.
-    threading: bool, optional
-        Try threading the process for speedup. Default is false.
-    num_threads: int, optional
-        Number of threads for calculating the running mean.  Only
-        valid if threading is set to True.  If value is not 
-        specified and threading is requested, the default value 
-        is 4.
+    num_procs: int, optional
+        Number of processes for calculating the running mean.  
+        The default value is 8. Only valid for useNCO=True.
     useNCO: bool, optional
         If the calculation is over a large dataset where memory use
         may be an issue, the use of netCDF operators is likely a good
-        idea.  It will speed up the process and save memory...
+        idea.  
 
     Returns
     -------
@@ -39,32 +39,106 @@ def runMean(nc_file, nc_varname, window_size, threading=False, num_threads=None,
         Number of elements removed from the ending of the time series
     """
 
-    if threading and num_threads is None:
-        num_threads = 4
 
-    #Calculate edges of running mean calculatable from given data
-    data = nc_file.variables[nc_varname].data
+    _DEBUG = False
+    if num_procs is not None:
+        PROCESSES = num_procs
+    else: PROCESSES = 8
+
+    #Load NetCDF data.  Try with scale_factor and offset first.
+    nc_var = nc_file.variables[nc_varname]
+    try:
+        sf = nc_var.scale_factor
+        offset = nc_var.add_offset
+        data = nc_var.data * sf + offset
+    except AttributeError as e:
+        data = nc_var.data
+      
     dshape = data.shape
     top_edge = window_size/2 + 1
     bot_edge = (window_size/2) + (window_size%2) - 1
-    tot_cut = top_edge + bot_edge
+    tot_cut = top_edge + bot_edge  #TODO: fix off by one for even numbers
     new_shape = list(dshape)
     new_shape[0] -= tot_cut
 
+    #Use netCDF operators on system instead of python based running mean (saves
+    #  memory).  For now I suggest using this method.
     if useNCO:
-        inFile = nc_file.filename 
-        for idx in range(new_shape[0]):
-            outFile = '.'.join([inFile, str(idx), 'tmp'])
-            execArgs = ['ncra', '-d', 'time,%i,%i'%(idx, idx+window_size), inFile, outFile]
-            p = subprocess.Popen(execArgs)
-            p.wait()
+        inFile = nc_file.filename
+        inFile_no_ext,file_ext = splitext(inFile)
+        pad_len = len(str(new_shape[0])) #calculate padding for tmp file numbers
+        fmt_str = '%s0%ii' % ('%', pad_len)
+        wildcard = '?'*pad_len
+        out_file = '.'.join([inFile_no_ext, '%imon_runmean' % window_size, file_ext])
+
+        if _DEBUG:
+            print 'inFile: %s' % inFile
+            print 'out_file: %s' % out_file
+            print 'file_ext: %s' % file_ext
+            print 'inFile_no_ext: %s' % inFile_no_ext
+
+        #Check if running mean was already done, prompt to recalculate
+        if exists(out_file):
+            user_in = ''
+            while user_in != 'y' and user_in != 'n':
+                user_in = raw_input('Would you like to recalculate the running' 
+                                     ' mean?[y or n]: ').rstrip()
+            if user_in == 'n':
+                f = ncf.netcdf_file(out_file, 'r')
+                result = f.variables[nc_varname].data
+                return (result, bot_edge, top_edge)
+
+        #Calculate the running average
+        pool = Pool(PROCESSES)
+        cmds = [ ['ncra',
+                  '-O', 
+                  '-d', 
+                  'time,%i,%i' % (idx, idx+window_size-1),
+                  inFile,
+                  '.'.join([inFile_no_ext, fmt_str % idx, 'tmp']) #temporary out file
+                  ] for idx in range(new_shape[0])
+                ]
+        for i,retcode in enumerate(pool.imap_unordered(call, cmds, chunksize=100)):
+             if (i % (10**(pad_len-2)) == 0):
+                 print 'Processed %i of %i' % (i, new_shape[0])
+
+        #for idx in range(new_shape[0]):
+        #    if(idx % (10**(pad_len-2)) == 0):
+        #        print 'Calc run avg for idx %i' % idx
+        #    tmp_out = '.'.join([inFile_no_ext, fmt_str % idx, 'tmp'])
+        #    execArgs = ['ncra',
+        #                '-O',
+        #                '-d',
+        #                'time,%i,%i' % (idx, idx+window_size-1),
+        #                inFile,
+        #                tmp_out]
+        #    p = subprocess.Popen(execArgs)
+            #p.wait()
+
+        if _DEBUG: print cmds[0:2]
+
+        
+        #Concatenate tmp files
+        tmp_files = '.'.join([inFile_no_ext, wildcard, 'tmp'])
+        execArgs = ' '.join(['ncrcat', '-O', tmp_files, out_file])
+        p = Popen(execArgs, shell=True)
+        p.wait()
+
+        #Remove tmp files
+        execArgs = ' '.join(['rm', '-f', '.'.join([inFile_no_ext, '*.tmp'])])
+        p = Popen(execArgs, shell=True)
+        p.wait()
             
+        f = ncf.netcdf_file(out_file, 'r')
+        result = f.variables[nc_varname].data
+
     else:
         result = np.zeros(new_shape)
 
         for cntr in range(new_shape[0]):
-            print 'Calc for index %i' % cntr
-            result[cntr] = data[(cntr-bot_edge):(cntr+top_edge)].sum(axis=0)
+            if cntr % 100 == 0:
+                print 'Calc for index %i' % cntr
+            result[cntr] = data[(cntr):(cntr+top_edge+bot_edge)].sum(axis=0)
         result = result/float(window_size)
     
     return (result, bot_edge, top_edge)
