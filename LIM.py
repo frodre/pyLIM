0bc8237f6632b28f8e7a14e746c4d2c64abd1946
Andre Perkins <frodre@gmail.com>
2014-07-24 10:56:41 -0700

    Started new branch to work on error statistics of the LIM.  Also now ignores the figure files.

--- LIM.py
+++ LIM.py
@@ -25,7 +25,8 @@ plt.close('all')
 wsize = 12
 var_name = 'air'
 neigs = 20
-num_trials = 1
+num_trials = 150 
+forecast_tlim = 144 #months
 NCO = True #NetCDF Operators Flag, always false on Windows for now
 
 if os.name == 'nt':
@@ -34,7 +35,7 @@ if os.name == 'nt':
 else:
     #data_file = '/home/melt/wperkins/data/ccsm4_last_mil/tas_Amon_CCSM4_past1000_r1i1p1_085001-185012.nc'
     data_file = '/home/melt/wperkins/data/20CR/air.2m.mon.mean.nc'
-
+    
 #Load netcdf file
 f = ncf.netcdf_file(data_file, 'r')
 tvar = f.variables[var_name]
@@ -54,16 +55,17 @@ t2 = time()
 dur = t2 - t1
 print "Done! (Completed in %f s)" % dur
 
+#Calculate monthly climatology
 new_shp = [run_mean.shape[0]/wsize, wsize] + list(run_mean.shape[1:])
 shp_run_mean = run_mean.reshape( new_shp )
-
 print "\nCalculating monthly climatology from running mean..."
 mon_climo = np.sum(shp_run_mean, axis=0)/float(new_shp[0])
 print "Done!"
 
+#Remove the monthly climo from the running mean
 anomaly_srs = (shp_run_mean - mon_climo).reshape(([new_shp[0]*wsize] + new_shp[2:]))
 
-#Reshape data for covariance
+#Calculate EOFs
 shp = anomaly_srs.shape
 shp_anomaly = anomaly_srs.reshape(shp[0], shp[1]*shp[2])
 print "\nCalculating EOFs..."
@@ -76,41 +78,74 @@ eof_proj = np.dot(eofs.T, shp_anomaly.T)
 print "\nLeading %i EOFS explain %f percent of the total variance" % (neigs, var_pct)
 
 #Start running trials for LIM forecasts
-for i in range(num_trials):
-    print """
-=========
-Trial %i
-=========
-    """ % i
-    #randomize sample of indices for training and testing
-    tlimit = 12*12
-    time_dim = eof_proj.shape[1] - tlimit
-    tsample = int(time_dim*0.9)
-    rnd_idx = sample(xrange(time_dim), time_dim)
+loc = 100 # just for testing right now
+time_dim = eof_proj.shape[1] - forecast_tlim
+tsample = int(time_dim*0.9)
+forecasts = np.zeros( [num_trials, forecast_tlim+1, neigs, (time_dim - tsample)] )
+fcast_idxs = np.zeros ((num_trials, (time_dim - tsample)),
+                        dtype=np.int16)
 
-    for tau in range(tlimit):
-        train_idx = np.array(rnd_idx[0:tsample])
+for i in range(num_trials):
+    print 'Running trial %i' % (i+1)
+    #randomize sample of indices for training and testing
+    rnd_idx = sample(xrange(time_dim), time_dim)
+    train_idx = np.array(rnd_idx[0:tsample])
+    indep_idx = np.array(rnd_idx[tsample:])
+    fcast_idxs[i] = indep_idx
+
+    for tau in range(forecast_tlim+1):
         x0 = eof_proj[:,train_idx]
         xt = eof_proj[:,(train_idx + tau)]
         xtx0 = np.dot(xt,x0.T) 
         x0x0 = np.dot(x0, x0.T)
         M = np.dot(xtx0, np.linalg.pinv(x0x0))
 
-        #Test on training data
-        xf = np.dot(M, x0)
-        err = xf - xt
-        if (tau + 1) % 12 == 0:
-            print 'Mean training set error: %f' % abs(err).mean()
-
         #Test on independent data
-        indep_idx = np.array(rnd_idx[(tsample+1):])
         x0i = eof_proj[:,indep_idx]
         xti = eof_proj[:,(indep_idx + tau)]
         xfi = np.dot(M, x0i)
-        erri = xfi - xti
+        forecasts[i, tau] = xfi
+   
+#Move into physical space for error calc
+#forecast = np.dot(eofs, xfi)[loc]
+#true_space = shp_anomaly.T[loc, indep_idx + tau]
+#erri = forecast - true_space
+#error_stats[i, tau, :] = erri
 
-        if (tau + 1) % 12 == 0:
-            print 'Mean training set error: %f' % abs(err).mean()
-            print 'Mean testing set error: %f' % abs(erri).mean()
-         
+#Verification Stuff
+lead_times = np.array([0, 1, 3, 6, 9, 12])*12
+anom_truth = lambda x:  np.array([shp_anomaly.T[loc, (fcast_idxs[i] + x)] for i in len(fcast_idxs)])
+true_var = np.array([anom_truth(tau).var() for tau in lead_times]) 
+true_mean = np.array([anom_truth(tau).mean() for tau in lead_times])
+
+fcast_var = np.zeros( (len(lead_times), num_trials) )
+fcast_mean = np.zeros( fcast_var.shape )
+
+for i in range(len(lead_times)):
+     loc_fcast = np.array([np.dot(eofs[loc], fcast[lead_times[i]]) for fcast in forecasts])
+     varis = np.zeros( len(loc_fcast) )
+     means = np.zeros( varis.shape )
+     for j in range(len(loc_fcast)):
+         varis[j] = loc_fcast[0:j+1].var() 
+         means[j] = loc_fcast[0:j+1].mean()
+     fcast_var[i,:] = varis
+     fcast_mean[i,:] = means
+
+fig, ax = plt.subplots(2,1, sharex=True)
+x = range(fcast_var.shape[1])
+
+for i,var in enumerate(fcast_var):
+    ax[0].plot(x, var, linewidth=2, label='Lead Time = %i yr' % (lead_times[i]/12))
+    ax[0].legend()
+    ax[1].plot(x, fcast_mean[i], linewidth=2)
+
+for line, var in zip(ax[0].get_lines(), true_var):
+    ax[0].axhline(y=var, linestyle = '--', color = line.get_color())
+
+ax[0].set_title('Forecast Variance & Mean vs. # Trials (Single Gridpoint)')
+ax[1].set_xlabel('Trial #')
+ax[0].set_ylabel('Variance (K)')
+ax[1].set_ylabel('Mean (K)')
+
+fig.show()
 f.close()
